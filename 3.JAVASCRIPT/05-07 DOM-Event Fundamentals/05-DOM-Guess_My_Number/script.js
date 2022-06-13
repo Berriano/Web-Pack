47dc396f19f238957f3299260d8d06aec82f58cf
Berriano <beresford2102@yahoo.com.au>
2022-06-14 07:48:38 +1000

    newStructure

---
+++ 3.JAVASCRIPT/05-07 DOM-Event Fundamentals/05-DOM-Guess_My_Number/script.js
@@ -0,0 +1,39 @@
+// ------------------------    DOM and Event    ----------------------------//
+// --------------------------   Fundamentals   -----------------------------//
+
+
+
+'use strict';
+
+
+
+//--------------------------  querySelector() ------------------------------//
+
+
+
+/// to select class and id tags and return the first element
+/// for the given CSS selector
+
+
+///?  console.log(document.querySelector('.message'));
+
+
+//   <p class="message">Start guessing...</p>
+
+
+
+///  to then get the text content of the element
+
+
+
+console.log(document.querySelector('.message').textContent);
+
+
+//   Start guessing...
+
+
+
+
+
+
+
