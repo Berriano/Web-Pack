b4922b10eba79d49b8d19d4341eee84840b57a1a
Berriano <beresford2102@yahoo.com.au>
2022-06-13 16:01:19 +1000

    new

--- 3.JAVASCRIPT/03-Developer-Skills/script.js
+++ 3.JAVASCRIPT/03-Developer-Skills/script.js
@@ -1,3 +1,25 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+
+
+const tempsArr = [12, 5, -5, 0, 4];
+
+
+
+const printForecast = function(arr) {	
+
+    let string = '...';
+
+    for (let i = 0; i < arr.length; i++) {
+
+        string += ` ${arr[i]} degrees in ${i + 1} days ... `;
+
+    }
+		
+    return string;
+
+}
+
+
+console.log(printForecast(tempsArr));
